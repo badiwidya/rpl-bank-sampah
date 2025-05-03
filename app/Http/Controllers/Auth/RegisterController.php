1b45a2ae253344fe3b2630eebbef2bd6c6102272
mixinit <adiwidya.berton13@gmail.com>
2025-05-04 03:19:52 +0700

    Buat method store (register) di controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
 
 class RegisterController extends Controller
 {
@@ -20,6 +23,18 @@ class RegisterController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'nama' => 'required|string|min:3|max:255',
+            'email' => 'required|email|unique:users,email',
+            'no_telepon' => 'required|unique:users,no_telepon|regex:/^08[0-9]{8,11}$/',
+            'password' => ['required', 'confirmed', Password::min(8)->letters()->numbers()],
+        ]);
+
+        $user = User::create($validated);
+
+        $user->profile()->create();
+
+        Auth::login($user);
+
     }
 }
