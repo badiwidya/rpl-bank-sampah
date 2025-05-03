9ea349bc1b25f4d5439c79ca40f1082f5f2ce7ca
mixinit <adiwidya.berton13@gmail.com>
2025-05-04 02:44:30 +0700

    Tambah rute untuk register
    digroup dengan nama nasabah. (belum termasuk controller)

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::name('nasabah.')->group(function () {
+    Route::get('/register')->name('register.show');
+    Route::post('/register')->name('register.submit');
+});
