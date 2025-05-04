f09139de5d68cc01d34953a01ea90a5b070cce93
mixinit <adiwidya.berton13@gmail.com>
2025-05-04 10:02:30 +0700

    Tambah route yang dibutuhin

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,9 @@ Route::name('nasabah.')->group(function () {
     Route::get('/register', [RegisterController::class, 'create'])->name('register.show');
     Route::post('/register', [RegisterController::class, 'store'])->name('register.submit');
 });
+
+Route::prefix('email')->name('mail.')->group(function () {
+    Route::get('/verify')->name('verification.notice');
+    Route::get('/verify/{hash}/{id}')->name('verification.verify');
+    Route::post('/verify')->name('verification.resend');
+});
