51314c25fefb1cdc6a60b71ae4c0a6927854662c
mixinit <adiwidya.berton13@gmail.com>
2025-05-04 17:26:23 +0700

    Buat controller (SessionController) sama tulis dulu rute yang dibutuhin

---
+++ app/Http/Controllers/Auth/SessionController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SessionController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
