733227f19127327469a7271704d56b51bf8eabcd
mixinit <adiwidya.berton13@gmail.com>
2025-05-04 21:50:06 +0700

    Buat contoh view untuk reset password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,3 +1,15 @@
 <div>
-    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
+    <form action="{{ route('auth.password.email') }}" method="post">
+        @csrf
+        <label for="email">Please enter your email address: </label>
+        <input type="email" name="email" id="email">
+        <button type="submit">Reset Password</button>
+    </form>
+    @foreach ($errors->all() as $error)
+        <p>{{ $error }}</p>
+    @endforeach
+
+    @if (session('status'))
+        <p>{{ session('status') }}</p>
+    @endif
 </div>
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,3 +1,20 @@
 <div>
-    <!-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius -->
+    <form action="{{ route('auth.password.update') }}" method="post">
+        @csrf
+        <input type="hidden" name="token" value="{{ $token }}">
+        <label for="email">Email: </label>
+        <input type="email" name="email" id="email" value="{{ $email }}" readonly>
+        <br>
+        <label for="password">New Password: </label>
+        <input type="password" name="password" id="password">
+        <br>
+        <label for="password_confirmation">Confirm New Password: </label>
+        <input type="password" name="password_confirmation" id="password_confirmation">
+
+        <button type="submit">Change Password</button>
+    </form>
+
+    @foreach ($errors->all() as $error)
+        <p>{{ $error }}</p>
+    @endforeach
 </div>
