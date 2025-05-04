40e34fe4336a9c4eaae2455af2c1daf46c62ac20
mixinit <adiwidya.berton13@gmail.com>
2025-05-04 21:48:51 +0700

    Tulis logika untuk update password

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Validation\Rules\Password as RulesPassword;
+use Illuminate\Support\Str;
 
 class ForgotPasswordController extends Controller
 {
@@ -46,8 +51,28 @@ class ForgotPasswordController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => ['required', 'confirmed', RulesPassword::min(8)->letters()->numbers()],
+        ]);
+
+        $status = Password::reset($validated, function (User $user, string $password) {
+            $user->forceFill([
+                'password' => Hash::make($password),
+            ])->setRememberToken(Str::random(60));
+
+            $user->save();
+
+            event(new PasswordReset($user));
+        });
+
+        $user = User::where('email', $request->email)->first();
+
+        return $status === Password::PasswordReset
+            ? redirect()->route($user->role . '.login.show')->with('status', __($status))
+            : back()->withErrors(['email' => __($status)]);
     }
 }
