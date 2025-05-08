3886f537e8707aed19c3c262f4fc68c0d613a01b
mixinit <adiwidya.berton13@gmail.com>
2025-05-08 21:46:57 +0700

    Buat method store di NasabahProfileController
    kocak, logikanya sama ternyata, baru sadar gua. Tau gitu gausah bikin dua controller. Tambahin aja kondisionalnya

--- app/Http/Controllers/Nasabah/NasabahProfileController.php
+++ app/Http/Controllers/Nasabah/NasabahProfileController.php
@@ -3,12 +3,42 @@
 namespace App\Http\Controllers\Nasabah;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateProfileRequest;
+use App\Services\ProfileService;
 use Illuminate\Http\Request;
 
 class NasabahProfileController extends Controller
 {
+    public function __construct(
+        protected ProfileService $profileService
+    ) {}
+
     public function create()
     {
         return view('nasabah.profile');
+    }
+
+    public function store(UpdateProfileRequest $request)
+    {
+        $user = $request->user();
+
+        $validated = $request->safe()->except(['email']);
+
+        $email = $request->safe()->email;
+
+        if ($request->hasFile('avatar')) {
+            $this->profileService->updateAvatar($user, $request->file('avatar'));
+        }
+
+        $changedEmail = $this->profileService->updateEmail($user, $email);
+
+        $user->update($validated);
+
+        return back()->with([
+            'success' => 'Informasi profil Anda telah diperbarui.',
+            'email' => $changedEmail 
+                ? 'Silakan periksa email baru Anda untuk mengganti email, link verifikasi yang diberikan hanya akan bertahan selama 60 menit.' 
+                : ''
+        ]);
     }
 }
