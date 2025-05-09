6629c13ab94b688b0a1ac850579798215f8ce219
mixinit <adiwidya.berton13@gmail.com>
2025-05-09 20:54:36 +0700

    Tambah controller untuk ganti psasword

---
+++ app/Http/Controllers/Auth/ChangePasswordController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\ValidationException;
+
+class ChangePasswordController extends Controller
+{
+    public function create()
+    {
+
+    }
+
+    public function update(Request $request)
+    {
+
+    }
+}
