6eb01e0b4600edb07cd97b858a008ecf068be5d2
mixinit <adiwidya.berton13@gmail.com>
2025-05-15 00:01:37 +0700

    Buat hover area di nav menu header lebih luas

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,11 +1,11 @@
-<header class="flex justify-between items-center px-8 py-4 bg-white drop-shadow-md">
-    <a href="/" class="flex items-center space-x-2 font-semibold text-lg">
+<header class="flex justify-between items-center bg-white drop-shadow-md">
+    <a href="/" class="flex items-center space-x-2 font-semibold text-lg px-8 py-4">
         <img src="{{ asset('/assets/bank-sampah-logo.svg') }}" alt="Logo bank sampah" class="w-6 h-6"/>
         <span>Bank Sampah</span>
     </a>
-    <nav class="space-x-6 text-sm font-medium">
-        <a href="#" class="text-gray-700 hover:text-green-600">Home</a>
-        <a href="#" class="text-gray-700 hover:text-green-600">About Us</a>
-        <a href="#" class="text-gray-700 hover:text-green-600">Profile</a>
+    <nav class="flex h-full text-sm font-medium pr-8">
+        <a href="#" class="px-4 flex items-center text-gray-700 hover:text-green-400 hover:underline transition duration-300">Home</a>
+        <a href="#" class="px-4 flex items-center text-gray-700 hover:text-green-400 hover:underline transition duration-300">About Us</a>
+        <a href="#" class="px-4 flex items-center text-gray-700 hover:text-green-400 hover:underline transition duration-300">Profile</a>
     </nav>
 </header>
