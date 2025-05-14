864b5298f4faf626d633bf7e6c14b60e1b49503c
mixinit <adiwidya.berton13@gmail.com>
2025-05-15 00:42:42 +0700

    Ubah size teks placeholder

--- resources/views/auth/admin-login.blade.php
+++ resources/views/auth/admin-login.blade.php
@@ -36,7 +36,7 @@
                 <div class="mb-4">
                     <label class="block text-sm font-medium text-gray-700 mb-1">Email/No. Telepon</label>
                     <input type="text" placeholder="Masukkan Email/No. Telepon" name="login"
-                           class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-green-400 transition duration-300 @error('login') border-red-500 focus:ring-red-500 @enderror"/>
+                           class="w-full px-4 py-2 placeholder:text-sm border rounded-md focus:outline-none focus:ring-2 focus:ring-green-400 transition duration-300 @error('login') border-red-500 focus:ring-red-500 @enderror"/>
                     @error('login')
                     <div class="text-red-500 text-xs mt-1">{{ $message }}</div>
                     @enderror
@@ -47,7 +47,7 @@
                     <label class="block text-sm font-medium text-gray-700 mb-1">Password</label>
                     <div class="relative">
                         <input type="password" placeholder="Masukkan Password" name="password"
-                               class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-green-400 transition duration-300 @error('password') border-red-500 focus:ring-red-500 @enderror"/>
+                               class="w-full px-4 py-2 placeholder:text-sm border rounded-md focus:outline-none focus:ring-2 focus:ring-green-400 transition duration-300 @error('password') border-red-500 focus:ring-red-500 @enderror"/>
                         <div class="absolute inset-y-0 right-3 flex items-center cursor-pointer toggle-password">
                             <!-- Eye Open -->
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-400 eye-open"
