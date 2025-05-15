c032a9f9781e8fa54db1cdce510405a289564b32
mixinit <adiwidya.berton13@gmail.com>
2025-05-15 15:37:08 +0700

    Tambah old value ke login field

--- resources/views/auth/admin-login.blade.php
+++ resources/views/auth/admin-login.blade.php
@@ -36,7 +36,9 @@
                 <div class="mb-4">
                     <label class="block text-sm font-medium text-gray-700 mb-1">Email/No. Telepon</label>
                     <input type="text" placeholder="Masukkan Email/No. Telepon" name="login"
-                           class="w-full px-4 py-2 placeholder:text-sm border rounded-md focus:outline-none focus:ring-2 focus:ring-green-400 transition duration-300 @error('login') border-red-500 focus:ring-red-500 @enderror"/>
+                           class="w-full px-4 py-2 placeholder:text-sm border rounded-md focus:outline-none focus:ring-2 focus:ring-green-400 transition duration-300 @error('login') border-red-500 focus:ring-red-500 @enderror"
+                           value="{{ old('login') }}"
+                    />
                     @error('login')
                     <div class="text-red-500 text-xs mt-1">{{ $message }}</div>
                     @enderror
