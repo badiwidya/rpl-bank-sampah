746c806ab6613d09b55ba20d389438c28cb44e1a
mixinit <adiwidya.berton13@gmail.com>
2025-05-16 17:13:37 +0700

    Tambah beberapa method dan implementasi orderby

--- app/Livewire/KatalogSampah.php
+++ app/Livewire/KatalogSampah.php
@@ -4,18 +4,40 @@ namespace App\Livewire;
 
 use App\Models\Sampah;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class KatalogSampah extends Component
 {
+    use WithPagination;
 
     public string $term = '';
+
+    public string $sortField = 'harga_per_kg';
+
+    public function sortBy($field)
+    {
+        if ($this->sortField === $field) {
+            $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc';
+        } else {
+            $this->sortField = $field;
+            $this->sortDirection = 'asc';
+        }
+    }
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
+    public string $sortDirection = 'asc';
+
     public function render()
     {
         $query = Sampah::query();
 
         $jenisSampah = $query->where('nama', 'like', '%' . $this->term . '%')
             ->orWhere('harga_per_kg', 'like', '%' . $this->term . '%')
-            ->latest()
+            ->orderBy($this->sortField, $this->sortDirection)
             ->paginate(10);
 
         return view('livewire.katalog-sampah', compact('jenisSampah'));
