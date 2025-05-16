a742277e402ed3c70ce69877f0203b157a95d979
mixinit <adiwidya.berton13@gmail.com>
2025-05-16 12:47:01 +0700

    Ganti jadi latest

--- app/Livewire/KatalogSampah.php
+++ app/Livewire/KatalogSampah.php
@@ -15,7 +15,7 @@ class KatalogSampah extends Component
 
         $jenisSampah = $query->where('nama', 'like', '%' . $this->term . '%')
             ->orWhere('harga_per_kg', 'like', '%' . $this->term . '%')
-            ->orderBy('created_at', 'desc')
+            ->latest()
             ->paginate(10);
 
         return view('livewire.katalog-sampah', compact('jenisSampah'));
