920f4ff3b189b79b172e2e5b747c09e63ad17dde
mixinit <adiwidya.berton13@gmail.com>
2025-05-16 12:33:37 +0700

    Bikin komponen livewire katalog-sampah

---
+++ app/Livewire/KatalogSampah.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class KatalogSampah extends Component
+{
+    public function render()
+    {
+        return view('livewire.katalog-sampah');
+    }
+}
