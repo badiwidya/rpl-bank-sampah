eca0155573aea07217c83dbb57089a353c9a8bfb
mixinit <adiwidya.berton13@gmail.com>
2025-05-16 12:31:34 +0700

    Buat policy untuk model Sampah

---
+++ app/Policies/SampahPolicy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Sampah;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class SampahPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Sampah $sampah): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->role === 'admin';
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Sampah $sampah): bool
+    {
+        return $user->role === 'admin';
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Sampah $sampah): bool
+    {
+        return $user->role === 'admin';
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Sampah $sampah): bool
+    {
+        return $user->role === 'admin';
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Sampah $sampah): bool
+    {
+        return $user->role === 'admin';
+    }
+}
