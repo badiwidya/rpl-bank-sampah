26ae001e45bbb5103ac6533bd3ea9be73694d75e
mixinit <adiwidya.berton13@gmail.com>
2025-05-16 17:30:17 +0700

    Bare minimum admin dashboard (logout) 😂

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,3 +1,8 @@
 <div>
-    <!-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius -->
+
+    <form action="{{ route('auth.logout') }}" method="post">
+        @csrf
+        <button type="submit">Logout</button>
+    </form>
+
 </div>
