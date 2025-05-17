e6a9a9f464cf2f51a64b51b80152c03437a91ca8
mixinit <adiwidya.berton13@gmail.com>
2025-05-18 02:52:31 +0700

    Buat validasi dan method update ganti password

--- app/Livewire/ChangePassword.php
+++ app/Livewire/ChangePassword.php
@@ -2,10 +2,52 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Redirect;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Masmerise\Toaster\Toaster;
 
 class ChangePassword extends Component
 {
+    public $currentPassword;
+
+    public $password;
+
+    public function rules()
+    {
+        return [
+            'currentPassword' => 'required|string',
+            'password' => ['required', 'string', 'confirmerd', Password::min(8)->letters()->numbers()]
+        ];
+    }
+
+    public function update()
+    {
+        $validated = $this->validate();
+        $user = Auth::user();
+        try {
+
+            if (!Hash::check($validated['currentPassword'], $user->password)) {
+                Toaster::error('Password lama Anda salah.');
+                return;
+            }
+
+            $user->update([
+                'password' => Hash::make($validated['password'])
+            ]);
+
+            return Redirect::route($user->role.'.dashboard.profile')
+                ->info('Password berhasil diganti!');
+
+        } catch (\Exception $e) {
+            Toaster::error('Terjadi kesalahan saat mengganti password.');
+        }
+    }
+
+    #[Title('Ganti Password - Bank Sampah')]
     public function render()
     {
         return view('livewire.change-password');
