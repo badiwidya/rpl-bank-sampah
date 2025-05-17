adf687c8eecc0655e3443a538b30d6cf20cb0f6a
mixinit <adiwidya.berton13@gmail.com>
2025-05-18 01:47:46 +0700

    Hapus controller dan view yang tidak perlu (sudah pakai livewire sekarang)

--- resources/views/admin/profile.blade.php
+++
@@ -1,7 +0,0 @@
-<x-layouts.app title="Pengaturan Profil - Bank Sampah">
-    
-    <div class="bg-gray-200 min-h-full flex justify-center items-center">
-        <livewire:user-profile-settings />
-    </div>
-
-</x-layouts.app>
--- resources/views/nasabah/profile.blade.php
+++
@@ -1,5 +0,0 @@
-<x-layouts.app title="Pengaturan Profil - Bank Sampah">
-
-    <livewire:nasabah-profile-settings />
-
-</x-layouts.app>
