d91511591f835ce2f010875b844cb7cf518fe76d
mixinit <adiwidya.berton13@gmail.com>
2025-05-18 14:34:11 +0700

    Penyesuaian di view komponen header

--- resources/views/livewire/components/header.blade.php
+++ resources/views/livewire/components/header.blade.php
@@ -2,7 +2,6 @@
 
     <header class="flex justify-between items-center bg-white drop-shadow-md z-50 fixed w-full">
         <div class="flex items-center px-4 py-4 space-x-2">
-            @if(auth()->check())
                 <button
                     @click="sidebarOpen = true"
                     class="p-2 text-emerald-600 hover:bg-gray-100 rounded-lg cursor-pointer"
@@ -14,10 +13,9 @@
                               stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
                     </svg>
                 </button>
-            @endif
 
             <!-- Logo -->
-            <a href="/" class="flex items-center space-x-2 font-semibold text-lg">
+            <a href="{{ route($user->role.'.dashboard.index') }}" class="flex items-center space-x-2 font-semibold text-lg">
                 <img src="{{ asset('assets/bank-sampah-logo.svg') }}" alt="Logo bank sampah" class="w-6 h-6"/>
                 <span>Bank Sampah</span>
             </a>
@@ -25,18 +23,17 @@
 
         <!-- Navbar -->
         <nav class="flex h-full text-sm font-medium pr-8">
-            <a href="#"
+            <a href="/"
                class="px-4 flex items-center text-gray-700 hover:text-emerald-400 hover:underline transition duration-300">Home</a>
-            <a href="#"
+            <a href="/about-us"
                class="px-4 flex items-center text-gray-700 hover:text-emerald-400 hover:underline transition duration-300">About
                 Us</a>
-            <a href="#"
+            <a href="/profile"
                class="px-4 flex items-center text-gray-700 hover:text-emerald-400 hover:underline transition duration-300">Profile</a>
         </nav>
     </header>
 
     <div class="py-8"></div>
-    @if(auth()->check())
         <!-- Overlay -->
         <div
             x-cloak
@@ -50,6 +47,7 @@
 
         <div
             x-cloak
+            x-data="{ role: '{{ $user->role }}' }"
             x-show="sidebarOpen"
             x-transition:enter="transition ease-out duration-300"
             x-transition:enter-start="-translate-x-full"
@@ -57,24 +55,29 @@
             x-transition:leave="transition ease-in duration-200"
             x-transition:leave-start="translate-x-0"
             x-transition:leave-end="-translate-x-full"
-            class="fixed text-white top-0 left-0 w-90 h-full bg-emerald-900 @if(auth()->user()->role === 'admin') bg-gray-800 @endif shadow-lg z-50 p-8 transform"
+            class="fixed text-white top-0 left-0 w-90 h-full shadow-lg z-50 p-8 transform"
+            :class="{
+                'bg-emerald-900': role === 'nasabah',
+                'bg-gray-800': role === 'admin'
+            }"
             @click.away="sidebarOpen = false"
         >
-            <a href="{{ route(auth()->user()->role.'.dashboard.profile') }}">
+            <a href="{{ route($user->role.'.dashboard.profile') }}">
                 <div class="flex items-center mb-4 gap-6">
                     <div class="rounded-full border-2 w-18 h-18 overflow-hidden">
-                        <img src="{{ asset(auth()->user()->avatar_url) }}"
-                             alt="{{ auth()->user()->nama_depan . ' profle picture' }}" class="object-cover h-full w-full">
+                        <img src="{{ asset($user->avatar_url) }}"
+                             alt="{{ $user->nama_depan . ' profle picture' }}" class="object-cover h-full w-full">
                     </div>
                     <div class="flex flex-col gap-1">
-                        <h1 class="text-white text-lg font-semibold">{{ auth()->user()->nama_depan.' '.auth()->user()->nama_belakang }}</h1>
-                        <p class="text-gray-200 text-xs font-light">{{ auth()->user()->role === 'admin' ? auth()->user()->email : 'Rp '.number_format(auth()->user()->profile->saldo, 0, ',', '.') }}</p>
+                        <h1 class="text-white text-lg font-semibold">{{ $user->nama_depan.' '.$user->nama_belakang }}</h1>
+                        <p class="text-gray-200 text-xs font-light">{{ $user->role === 'admin' ? $user->email : 'Rp '.number_format($user->profile->saldo, 0, ',', '.') }}</p>
                     </div>
                 </div>
             </a>
             <ul class="space-y- mt-6">
-                <li><a href="{{ route(auth()->user()->role.'.dashboard.profile') }}" class="block px-2 py-1 hover:underline rounded">Pengaturan Profil</a></li>
-                <li><a href="{{  route(auth()->user()->role.'.dashboard.sampah') }}" class="block px-2 py-1 hover:underline rounded">Katalog Sampah</a></li>
+                <li><a href="{{ route($user->role.'.dashboard.profile') }}" class="block px-2 py-1 hover:underline rounded">Dashboard</a></li>
+                <li><a href="{{ route($user->role.'.dashboard.profile') }}" class="block px-2 py-1 hover:underline rounded" wire:navigate>Pengaturan Profil</a></li>
+                <li><a href="{{  route($user->role.'.dashboard.sampah') }}" class="block px-2 py-1 hover:underline rounded" wire:navigate>Katalog Sampah</a></li>
                 <li>
                     <form id="logout-submit" action="{{  route('auth.logout') }}" method="post">
                         @csrf
@@ -83,5 +86,4 @@
                 </li>
             </ul>
         </div>
-    @endif
 </div>
