0cebccbff0cab4ded2f420e4a4ff57fcabe9edb9
mixinit <adiwidya.berton13@gmail.com>
2025-05-19 00:00:44 +0700

    penyesuaian riwayat-setoran

--- resources/views/livewire/riwayat-setoran.blade.php
+++ resources/views/livewire/riwayat-setoran.blade.php
@@ -18,7 +18,7 @@
         </div>
         @if (auth()->user()->role === 'admin')
             <a href="{{ route('admin.dashboard.setoran.create') }}" wire:navigate
-                class="block bg-emerald-600 hover:bg-emerald-700 drop-shadow-md hover:cursor-pointer text-white font-medium py-2 px-4 rounded-lg flex items-center transition duration-150 ease-in-out">
+                class="bg-emerald-600 hover:bg-emerald-700 drop-shadow-md hover:cursor-pointer text-white font-medium py-2 px-4 rounded-lg flex items-center transition duration-150 ease-in-out">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24"
                     stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
