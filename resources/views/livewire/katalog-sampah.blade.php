c3d878138e7c13969eff8c82950a2f77f6079d74
mixinit <adiwidya.berton13@gmail.com>
2025-05-18 15:05:40 +0700

    Sedikit penyesuaian

--- resources/views/livewire/katalog-sampah.blade.php
+++ resources/views/livewire/katalog-sampah.blade.php
@@ -269,7 +269,7 @@
                 <div class="mb-4">
                     <label class="block text-sm font-medium text-gray-700 mb-1">Nama Sampah</label>
                     <input type="text" name="nama" wire:model.defer="dataInput.nama"
-                           class="w-full px-4 py-2 placeholder:text-sm border rounded-md focus:outline-none focus:ring-2 focus:ring-emerald-400 transition duration-300 @error('dataInput.nama') border-red-500 focus:ring-red-500 @enderror"/>
+                           class="w-full border-gray-300 px-4 py-2 placeholder:text-sm border rounded-md focus:outline-none focus:ring-2 focus:ring-emerald-400 transition duration-300 @error('dataInput.nama') border-red-500 focus:ring-red-500 @enderror"/>
                     @error('dataInput.nama')
                     <div class="text-red-500 text-xs mt-1">{{ $message }}</div>
                     @enderror
@@ -277,7 +277,7 @@
 
                 <div class="mb-4">
                     <label class="block text-sm font-medium text-gray-700 mb-1">Harga per kg</label>
-                    <div class="flex items-center border rounded-md px-3 focus-within:ring-2 focus-within:ring-emerald-400 transition duration-300 @error('dataInput.harga_per_kg') border-red-500 focus-within:ring-red-500 @enderror">
+                    <div class="flex items-center border-gray-300 border rounded-md px-3 focus-within:ring-2 focus-within:ring-emerald-400 transition duration-300 @error('dataInput.harga_per_kg') border-red-500 focus-within:ring-red-500 @enderror">
                         <span class="text-gray-700 select-none">Rp.</span>
                         <input type="number" name="harga_per_kg" step="0.01"
                                wire:model.defer="dataInput.harga_per_kg"
