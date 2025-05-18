ce1b07ca132ba9e9d6bb0230d3b9cfe09dad1966
mixinit <adiwidya.berton13@gmail.com>
2025-05-18 11:46:43 +0700

    Intermezzo: buat accessor image url sampah

--- app/Models/Sampah.php
+++ app/Models/Sampah.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
@@ -10,15 +11,24 @@ class Sampah extends Model
     protected $table = 'sampah';
 
     protected $guarded = [];
-    
+
     public function transaksiPenukaran(): BelongsToMany
     {
         return $this->belongsToMany(
-            TransaksiPenukaran::class, 
-            'transaksi_penukaran_sampah', 
-            'sampah_id', 
+            TransaksiPenukaran::class,
+            'transaksi_penukaran_sampah',
+            'sampah_id',
             'transaksi_penukaran_id'
         )
             ->withPivot(['berat', 'harga_subtotal']);
+    }
+
+    protected function imageUrl(): Attribute
+    {
+        return Attribute::make(
+            get: function (string $value) {
+                return 'storage/' . $value;
+            }
+        );
     }
 }
