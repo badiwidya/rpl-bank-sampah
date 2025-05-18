e8813f6c633c39e0bed07a49bfc76c044efcf9db
mixinit <adiwidya.berton13@gmail.com>
2025-05-18 13:23:51 +0700

    Bikin dashboard layout

---
+++ resources/views/components/layouts/dashboard.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{ $title ?? 'Bank Sampah' }}</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @livewireStyles
+    </head>
+    <body class="font-poppins h-screen flex flex-col">
+        <x-header></x-header>
+        <main class="flex-1">
+            {{ $slot }}
+        </main>
+        <x-toaster-hub/>
+        @stack('scripts')
+        @livewireScripts
+    </body>
+</html>
