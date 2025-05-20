0a4a5328c1d262e07e883980e9436344b61f9db0
mixinit <adiwidya.berton13@gmail.com>
2025-05-21 06:56:47 +0700

    Buat relasi ke post sama booted static function

---
+++ app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
+
+class Category extends Model
+{
+    protected $guarded = [];
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    protected static function booted()
+    {
+        static::creating(function ($category) {
+            if (empty($category->slug)) {
+                $category->slug = Str::slug($category->name);
+            }
+        });
+    }
+}
