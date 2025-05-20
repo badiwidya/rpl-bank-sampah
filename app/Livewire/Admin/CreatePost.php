c5da3d5fea73470e5b6fedd62d1b1ae7c4393e5f
mixinit <adiwidya.berton13@gmail.com>
2025-05-21 06:57:54 +0700

    Define variabel yang dibutuhkan untuk buat postingan

--- app/Livewire/Admin/CreatePost.php
+++ app/Livewire/Admin/CreatePost.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class CreatePost extends Component
 {
+    public $postTitle;
+    public $categories;
+
+    public $categorySelected;
+    public $content;
+    public $coverImage;
+    
     public function render()
     {
         return view('livewire.admin.create-post');
