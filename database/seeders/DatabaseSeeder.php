88dfe81f3246a715c363bbbe8ec8b8e6b2436c65
mixinit <adiwidya.berton13@gmail.com>
2025-05-20 11:07:35 +0700

    Update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'nama_depan' => 'Makise',
+            'nama_belakang' => 'Kurisu',
+            'email' => 'admin@banksampah.com',
+            'password' => Hash::make('supersecret123'),
+            'role' => 'admin',
         ]);
+
+        User::factory(35)->create();
     }
 }
