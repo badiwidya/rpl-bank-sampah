01311e2ec6ef83b676895a903fc68defe361a753
mixinit <adiwidya.berton13@gmail.com>
2025-05-21 06:56:59 +0700

    Buat relasi ke category sama booted static function

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
@@ -12,11 +13,25 @@ class Post extends Model
 
     public function author(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'author_id');
     }
 
     public function images(): HasMany
     {
         return $this->hasMany(PostImage::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'post_id');
+    }
+
+    protected static function booted()
+    {
+        static::creating(function ($post) {
+            if (empty($post->slug)) {
+                $post->slug = Str::slug($post->name);
+            }
+        });
     }
 }
