3638d727b2e89a0756d4c8c14b124fab7041af1f
mixinit <adiwidya.berton13@gmail.com>
2025-05-20 22:38:41 +0700

    Buat variabel yang bakal ditampilin di dashboard admin

--- app/Livewire/Admin/Dashboard.php
+++ app/Livewire/Admin/Dashboard.php
@@ -2,12 +2,28 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\TransaksiPenukaran;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
+    public $user;
+    public $setoranBulanIni;
+    public $nasabahTotal;
+    public $sampahTotal;
+
+    public function mount()
+    {
+        $this->user = Auth::user();
+        $this->sampahTotal = TransaksiPenukaran::all()->sum('total_berat');
+        $this->nasabahTotal = User::where('role', 'nasabah')->count();
+        $this->setoranBulanIni = TransaksiPenukaran::whereMonth('created_at', now()->month)->count();
+    }
+
     #[Layout('components.layouts.dashboard')]
     #[Title('Dashboard - Bank Sampah')]
     public function render()
