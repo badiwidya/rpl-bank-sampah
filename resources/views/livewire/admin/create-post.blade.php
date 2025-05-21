feeac5ed8281f0da1b21ecca79d9da7d2943acdf
mixinit <adiwidya.berton13@gmail.com>
2025-05-21 07:31:11 +0700

    Buat kerangka view create post

--- resources/views/livewire/admin/create-post.blade.php
+++ resources/views/livewire/admin/create-post.blade.php
@@ -1,3 +1,16 @@
-<div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+<div class="bg-gradient-to-b from-emerald-50 to-emerald-200 min-h-full min-w-full flex justify-center items-center">
+
+    <div class="bg-white bg-opacity-90 flex flex-col backdrop-blur-md p-8 rounded-xl shadow-xl w-full max-w-[80%]">
+        <h1 class="font-semibold text-xl mb-4">Buat Postingan Baru</h1>
+
+        <label>Judul</label>
+        <input type="text" wire:model.blur="postTitle" placeholder="Masukkan judul postingan..."
+            class="w-full px-4 py-2 placeholder:text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-emerald-400 transition duration-300 @error('postTitle') border-red-500 focus:ring-red-500 @enderror" />
+        @error('postTitle')
+            <div class="text-sm text-red-400">{{ $message }}</div>
+        @enderror
+
+ 
+    </div>
+
 </div>
