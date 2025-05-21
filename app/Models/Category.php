a9c33a6a68d8206adf0269a99ba97dc36a4f8fb5
mixinit <adiwidya.berton13@gmail.com>
2025-05-21 17:17:46 +0700

    Fix relasi kategori sama Post

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,7 +12,7 @@ class Category extends Model
 
     public function posts(): HasMany
     {
-        return $this->hasMany(Post::class);
+        return $this->hasMany(Post::class, 'category_id');
     }
 
     protected static function booted()
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -23,7 +23,7 @@ class Post extends Model
 
     public function category(): BelongsTo
     {
-        return $this->belongsTo(Category::class, 'post_id');
+        return $this->belongsTo(Category::class, 'category_id');
     }
 
     protected static function booted()
