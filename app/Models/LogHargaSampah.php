d2be22c9d7e519436a4099a5cd0d047491111a0a
mixinit <adiwidya.berton13@gmail.com>
2025-05-22 20:03:41 +0700

    Define relasi ketiga model

--- app/Models/LogHargaSampah.php
+++ app/Models/LogHargaSampah.php
@@ -3,10 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LogHargaSampah extends Model
 {
     protected $table = 'log_harga_sampah';
 
     protected $guarded = [];
+
+    public function sampah(): BelongsTo
+    {
+        return $this->belongsTo(Sampah::class, 'sampah_id');
+    }
+
+    public function admin(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
